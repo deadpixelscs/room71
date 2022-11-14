550f1cfb96c951d9cd3bdf07868bfbb474b88ad6
Pete Machin <designer@deadpixelsdesign.com>
2022-11-14 20:02:15 +0000

    Add hyphen to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["room71.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["room-71.herokuapp.com", "localhost"]
 
 
 # Application definition
